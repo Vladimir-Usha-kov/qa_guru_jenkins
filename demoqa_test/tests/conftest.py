43cb598a39afc0919cce2b1eb7ff786e8b946e2d
Vladimir Ushakov <ushakov.vladimir98@mail.ru>
2024-11-18 13:09:14 +0400

    comm conftest

--- demoqa_test/tests/conftest.py
+++ demoqa_test/tests/conftest.py
@@ -6,26 +6,26 @@ from selene import Browser, Config, browser
 
 # from utils import attach
 
-@pytest.fixture(scope='function')
-def setup_browser(request):
-    options = Options()
-    selenoid_capabilities = {
-        "browserName": "chrome",
-        "browserVersion": "126.0",
-        "selenoid:options": {
-            "enableVNC": True,
-            "enableVideo": True
-        }
-    }
-    options.capabilities.update(selenoid_capabilities)
-    driver = webdriver.Remote(
-        command_executor=f"https://user1:1234@selenoid.autotests.cloud/wd/hub",
-        options=options
-    )
+# @pytest.fixture(scope='function')
+# def setup_browser(request):
+#     options = Options()
+#     selenoid_capabilities = {
+#         "browserName": "chrome",
+#         "browserVersion": "126.0",
+#         "selenoid:options": {
+#             "enableVNC": True,
+#             "enableVideo": True
+#         }
+#     }
+#     options.capabilities.update(selenoid_capabilities)
+#     driver = webdriver.Remote(
+#         command_executor=f"https://user1:1234@selenoid.autotests.cloud/wd/hub",
+#         options=options
+#     )
 
-    browser = Browser(Config(driver))
-    yield browser
-    browser.quit()
+    # browser = Browser(Config(driver))
+    # yield browser
+    # browser.quit()
 
 @pytest.fixture(scope='function', autouse=True)
 def browser_management():
