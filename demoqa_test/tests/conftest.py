04560fcba2d8f2ecf9c011dc74e21bf1885ba661
Vladimir Ushakov <ushakov.vladimir98@mail.ru>
2024-11-18 13:27:39 +0400

    dell chrdr conftest

--- demoqa_test/tests/conftest.py
+++ demoqa_test/tests/conftest.py
@@ -1,31 +1,9 @@
 import pytest
 
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from selene import Browser, Config, browser
 
-# from utils import attach
 
-# @pytest.fixture(scope='function')
-# def setup_browser(request):
-#     options = Options()
-#     selenoid_capabilities = {
-#         "browserName": "chrome",
-#         "browserVersion": "126.0",
-#         "selenoid:options": {
-#             "enableVNC": True,
-#             "enableVideo": True
-#         }
-#     }
-#     options.capabilities.update(selenoid_capabilities)
-#     driver = webdriver.Remote(
-#         command_executor=f"https://user1:1234@selenoid.autotests.cloud/wd/hub",
-#         options=options
-#     )
+from selene import  browser
 
-    # browser = Browser(Config(driver))
-    # yield browser
-    # browser.quit()
 
 @pytest.fixture(scope='function', autouse=True)
 def browser_management():
