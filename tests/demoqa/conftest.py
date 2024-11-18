147e4afda41a3c3b6a2f3e4ddaea5dfef0876fed
Vladimir Ushakov <ushakov.vladimir98@mail.ru>
2024-11-18 14:43:11 +0400

    add alure

--- tests/demoqa/conftest.py
+++ tests/demoqa/conftest.py
@@ -19,7 +19,7 @@ def setup_browser():
     options = Options()
     selenoid_capabilities = {
         "browserName": "chrome",
-        "browserVersion": "100.0",
+        "browserVersion": "126.0",
         "selenoid:options": {
             "enableVNC": True,
             "enableVideo": True
